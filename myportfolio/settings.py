330f6c4aeb3dcf1fb1befa950062512df820f21a
ankit <a.baliyan008@gmail.com>
2023-12-22 11:35:37 +0530

    update

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pstjfb3-k)^5xvhlp1d_n$-^lwu=xhb8@2l94$nraejc$mzfg8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ["discoverankit.azurewebsites.net"]
 ALLOWED_HOSTS = ["*"]
